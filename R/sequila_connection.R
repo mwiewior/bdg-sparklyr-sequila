a7c9b55678f1e4434885e36fcbc0641cfdce3ae2
Marek Wiewiórka <marek.wiewiorka@gmail.com>
2018-05-26 17:49:57 +0200

    fix session

--- R/sequila_connection.R
+++ R/sequila_connection.R
@@ -1,6 +1,7 @@
 sequila_connect <- function(master) {
   sc<- spark_connect(master=master ,config=sequilaEnv$config,version=sequilaEnv$sparkVersion,app_name = "SeQuiLa")
   session = sparklyr::invoke_static(sc, "org.biodatageeks.R.SequilaR","init")
+  ss <- new.env()
   ss$session <-session
   ss$sc <-sc
   ss
