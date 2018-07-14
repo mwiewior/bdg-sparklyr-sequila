e5a4a1171cd16377c7980b111a8ea3fa9f9cc13a
Marek Wiewiórka <marek.wiewiorka@gmail.com>
2018-07-14 12:01:14 +0200

    unit_test for connection

--- tests/testthat/test-sequila-conn-test.R
+++ tests/testthat/test-sequila-conn-test.R
@@ -2,9 +2,9 @@ context("test-sequila-conn-test.R")
 
 test_that("test SeQuiLa connection", {
   master <- "local[1]"
-  driver_mem="4g"
+  driver_mem="1g"
   ss<-sequila_connect(master,driver_memory=driver_mem)
   expect_equal(ss$sc$master, master)
   expect_equal(ss$sc$config$`sparklyr.shell.driver-memory`, driver_mem)
-  sequila_disconnect(ss)
+  #sequila_disconnect(ss)
 })
